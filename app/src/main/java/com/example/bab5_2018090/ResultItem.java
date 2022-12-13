6df0dbb22fcd852b87affa849893a233c84008a2
unknown <2018090@scholar.itn.ac.id>
2022-12-13 14:03:37 +0700

    Tugas 9 2018090

---
+++ app/src/main/java/com/example/bab5_2018090/ResultItem.java
@@ -0,0 +1,11 @@
+package com.example.bab5_2018090;
+
+public class ResultItem{
+    private String name;
+    private String url;
+
+    public String getName(){
+        return name;    }
+    public String getUrl(){        return url;
+    }
+}
